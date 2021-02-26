e1ad304985e7c6ff06a221b35d76887bb5a4cad1
Chris <chris.tomy.19@ucl.ac.uk>
2021-02-26 12:13:43 +0000

    draw image to screen, remove delay

--- main.cpp
+++ main.cpp
@@ -1,37 +1,77 @@
+#include "main.hpp"
 #include <iostream>
 #include <SDL2/SDL.h>
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 640;
 
-int main(int argc, char *argv[]) {
-    SDL_Window* window = nullptr;
-    SDL_Surface* screenSurface = nullptr;
+SDL_Window* gWindow = nullptr;
+SDL_Surface* gScreenSurface = nullptr;
+SDL_Surface* gHelloWorld = nullptr;
 
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+int main(int argc, char *argv[]) {
+    if (!init()) {
+        printf("Failed to init.\n");
         return 1;
     }
-    window = SDL_CreateWindow("SDL Practice",
+    if (!loadMedia()) {
+        printf("Failed to load media.\n");
+        return 1;
+    }
+
+    // applies image to surface.
+    // Draws to the back buffer.
+    SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+
+    // update front buffer, the thing the user sees
+    // NOTE: no delay is needed when running through command line, so I'll do
+    //       that from now on.
+    SDL_UpdateWindowSurface(gWindow);
+    SDL_Delay(2000);
+
+    close();
+    return 0;
+}
+
+bool init() {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return false;
+    }
+
+    gWindow = SDL_CreateWindow("SDL Practice",
                               SDL_WINDOWPOS_UNDEFINED,
                               SDL_WINDOWPOS_UNDEFINED,
                               SCREEN_WIDTH,
                               SCREEN_HEIGHT,
                               SDL_WINDOW_SHOWN);
-    if (window == NULL) {
+    if (gWindow == NULL) {
         printf("Couldn't create window! SDL_Error: %s\n", SDL_GetError());
-        return 1;
+        return false;
     }
 
-    screenSurface = SDL_GetWindowSurface(window);
-    SDL_Delay(700); // have to wait for a bit before filling rect
-    SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format,
-                                                 0xFF, 0xFF, 0xFF));
-    SDL_UpdateWindowSurface(window);
-    SDL_Delay(2000);
+    gScreenSurface = SDL_GetWindowSurface(gWindow);
 
-    SDL_DestroyWindow(window);
+    return true;
+}
+
+bool loadMedia() {
+    const char* relativePath = "assets/hello_world.bmp";
+    gHelloWorld = SDL_LoadBMP(relativePath);
+    if (gHelloWorld == NULL) {
+        printf("Unable to load image %s! SDL error: %s\n", relativePath, SDL_GetError());
+        return false;
+    }
+    return true;
+}
+
+void close() {
+    // deallocates surface
+    SDL_FreeSurface(gHelloWorld);
+    gHelloWorld = nullptr;
+
+    SDL_DestroyWindow(gWindow);
+    gWindow = nullptr;
+
     SDL_Quit();
-
-    return 0;
 }
---
+++ main.hpp
@@ -0,0 +1,12 @@
+#ifndef __MAIN_H_
+#define __MAIN_H_
+
+// starts SDL & creates window
+bool init();
+
+bool loadMedia();
+
+// frees media and closes SDL
+void close();
+
+#endif // __MAIN_H_
